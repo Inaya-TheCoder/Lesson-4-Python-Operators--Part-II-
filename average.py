bc96e301e47043b8ba21371914a6e9ad71b71294
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-08-22 21:21:32 +0530

    average

---
+++ average.py
@@ -0,0 +1,8 @@
+candy1 = 7
+candy2 = 5
+candy3 = 9
+candy4 = 10
+sum = candy1 + candy2 + candy3 + candy4 
+print ("The sum of the candies is",sum)
+average = sum / 4
+print ("The average of the candies is", average)
