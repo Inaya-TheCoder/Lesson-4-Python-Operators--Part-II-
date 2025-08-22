8a03e5f5b311e66ee4f1fc9e3357d050f1124c69
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-08-22 21:51:01 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,9 @@
+print("Enter the marks in the following subjects-")
+Math = int(input("YOUR MARKS IN MATHS ARE:"))
+English = int(input("YOUR MARKS IN ENGLISH ARE:"))
+Sst = int(input("YOUR MARKS IN SST ARE:"))
+Science = int(input("YOUR MARKS IN SCIENCE ARE:"))
+sum = Math + Science + Sst + English
+print("SUM OF THE SUBJECTS IS-")
+PERCENTAGE = (sum/200)*100
+print("YOUR PERCENTAGE IS",PERCENTAGE)
